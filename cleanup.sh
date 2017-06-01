51be5d64c714ff72b639cd2590edf1afbac5a562
Daniel Bachhuber <daniel@handbuilt.co>
2017-06-01 14:34:09 -0700

    Safely, but forcefully, delete the write-protected .git directory

--- cleanup.sh
+++ cleanup.sh
@@ -12,6 +12,8 @@ bash check-env.sh
 # @todo Reset the database to its original condition by dropping tables
 
 # Clean up the preparation directory
+# Only forcefully delete the .git directory, to prevent disasters otherwise
+rm -rf $WPT_PREPARE_DIR/.git
 rm -r $WPT_PREPARE_DIR
 
 # Delete all files delivered to the test environment.
