0f53a758b80f950b00a69feb317f15b23fb85410
Daniel Bachhuber <daniel@handbuilt.co>
2017-06-01 14:46:42 -0700

    Generate a `wp-tests-config.php` based on env variables

--- prepare.sh
+++ prepare.sh
@@ -23,7 +23,17 @@ git clone --depth=1 git@github.com:WordPress/wordpress-develop.git $WPT_PREPARE_
 # Download phpunit.phar
 wget -O $WPT_PREPARE_DIR/phpunit.phar https://phar.phpunit.de/phpunit-5.7.phar
 
-# @todo Generate wp-tests-config.php
+# Generate wp-tests-config.php
+cp "$WPT_PREPARE_DIR"/wp-tests-config-sample.php "$WPT_PREPARE_DIR"/wp-tests-config.php
+if [[ $(uname -s) == 'Darwin' ]]; then
+	local ioption='-i .bak'
+else
+	local ioption='-i'
+fi
+sed $ioption "s/youremptytestdbnamehere/$WPT_DB_NAME/" "$WPT_PREPARE_DIR"/wp-tests-config.php
+sed $ioption "s/yourusernamehere/$WPT_DB_USER/" "$WPT_PREPARE_DIR"/wp-tests-config.php
+sed $ioption "s/yourpasswordhere/$WPT_DB_PASS/" "$WPT_PREPARE_DIR"/wp-tests-config.php
+sed $ioption "s|localhost|${WPT_DB_HOST}|" "$WPT_PREPARE_DIR"/wp-tests-config.php
 
 # Deliver all files to test environment
 rsync -rv --exclude='.git/' $WPT_PREPARE_DIR $WPT_SSH_CONNECT:$WPT_TARGET_DIR
