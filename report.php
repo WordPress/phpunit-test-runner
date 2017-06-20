453c1440e1475f3dc54482fd05318e14eeb41fb0
Daniel Bachhuber <daniel@handbuilt.co>
2017-06-20 15:24:38 -0700

    Replace report.sh with report.php

---
+++ report.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Reports the test run results to WordPress.org
+ */
+
+require __DIR__ . '/functions.php';
+
+// Check required environment variables
+check_required_env();
+
+// @todo process phpunit test run results; send to WordPress.org
