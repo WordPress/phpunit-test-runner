ee4ac7b47d2780bc7f6623c9bc92281b2f225be4
Javier Casares <javier@casares.org>
2024-03-16 17:16:57 +0100

    test for multi-php

--- test.php
+++ test.php
@@ -25,6 +25,7 @@ $WPT_SSH_CONNECT    = trim( getenv( 'WPT_SSH_CONNECT' ) );
 $WPT_TEST_DIR       = trim( getenv( 'WPT_TEST_DIR' ) );
 $WPT_SSH_OPTIONS    = trim( getenv( 'WPT_SSH_OPTIONS' ) ) ? : '-o StrictHostKeyChecking=no';
 $WPT_PHP_EXECUTABLE = trim( getenv( 'WPT_PHP_EXECUTABLE' ) ) ? : 'php';
+$WPT_PHP_EXECUTABLE_MULTI = trim( getenv( 'WPT_PHP_EXECUTABLE_MULTI' ) ) ? : '';
 
 // Uses the flavor (usually to test WordPress Multisite)
 $WPT_FLAVOR_INI = trim( getenv( 'WPT_FLAVOR' ) );
@@ -63,23 +64,84 @@ switch( $WPT_EXTRATESTS_INI ) {
 unset( $WPT_EXTRATESTS_INI );
 
 /**
- * Determines the PHPUnit command to execute the test suite.
- * Retrieves the PHPUnit command from the environment variable 'WPT_PHPUNIT_CMD'. If the environment
- * variable is not set or is empty, it constructs a default command using the PHP executable path and
- * the test directory path from environment variables, appending parameters to the PHPUnit call to
- * avoid reporting useless tests.
  */
-$WPT_PHPUNIT_CMD = trim( getenv( 'WPT_PHPUNIT_CMD' ) );
-if( empty( $WPT_PHPUNIT_CMD ) ) {
-	$WPT_PHPUNIT_CMD = 'cd ' . escapeshellarg( $WPT_TEST_DIR ) . ' && ' . $WPT_PHP_EXECUTABLE . ' ./vendor/phpunit/phpunit/phpunit --dont-report-useless-tests' . $WPT_FLAVOR_TXT . $WPT_EXTRATESTS_TXT;
+$WPT_PHP_EXECUTABLE_MULTI_ARRAY = array();
+if ( '' !== $WPT_PHP_EXECUTABLE_MULTI ) {
+
+	$php_multi_versions = explode( ',', $WPT_PHP_EXECUTABLE_MULTI );
+
+	foreach( $php_multi_versions as $php_multi_version ) {
+
+		$php_multi_v = explode( '+', $php_multi_version );
+
+		if( isset( $php_multi_v[0] ) && $php_multi_v[0] && isset( $php_multi_v[1] ) && $php_multi_v[1] ) {
+			$WPT_PHP_EXECUTABLE_MULTI_ARRAY[] = array( 'version' => trim( $php_multi_v[0] ), 'bin' => trim( $php_multi_v[1] ) );
+		}
+
+		unset( $php_multi_version );
+	}
+	
+	unset( $php_multi_versions );
 }
 
-// If an SSH connection string is provided, prepend the SSH command to the PHPUnit execution command.
-if ( ! empty( $WPT_SSH_CONNECT ) ) {
-	$WPT_PHPUNIT_CMD = 'ssh ' . $WPT_SSH_OPTIONS . ' ' . escapeshellarg( $WPT_SSH_CONNECT ) . ' ' . escapeshellarg( $WPT_PHPUNIT_CMD );
-}
+if( count( $WPT_PHP_EXECUTABLE_MULTI_ARRAY ) ) {
 
-// Execute the PHPUnit command.
-perform_operations( array(
-	$WPT_PHPUNIT_CMD
-) );
+/**
+ * Performs a series of operations to set up the test environment. This includes creating a preparation directory,
+ * cloning the WordPress development repository, downloading the WordPress importer plugin, and preparing the environment with npm.
+ */
+// Prepare an array of shell commands to set up the testing environment.
+
+	foreach( $WPT_PHP_EXECUTABLE_MULTI_ARRAY as $php_multi ) {
+
+		$WPT_TEST_DIR_MULTI = $WPT_TEST_DIR . '-' . crc32( $php_multi['version'] );
+
+		/**
+		 * Determines the PHPUnit command to execute the test suite.
+		 * Retrieves the PHPUnit command from the environment variable 'WPT_PHPUNIT_CMD'. If the environment
+		 * variable is not set or is empty, it constructs a default command using the PHP executable path and
+		 * the test directory path from environment variables, appending parameters to the PHPUnit call to
+		 * avoid reporting useless tests.
+		 */
+		$WPT_PHPUNIT_CMD = trim( getenv( 'WPT_PHPUNIT_CMD' ) );
+		if( empty( $WPT_PHPUNIT_CMD ) ) {
+			$WPT_PHPUNIT_CMD = 'cd ' . escapeshellarg( $WPT_TEST_DIR_MULTI ) . ' && ' . $php_multi['bin'] . ' ./vendor/phpunit/phpunit/phpunit --dont-report-useless-tests' . $WPT_FLAVOR_TXT . $WPT_EXTRATESTS_TXT;
+		}
+
+		// If an SSH connection string is provided, prepend the SSH command to the PHPUnit execution command.
+		if ( ! empty( $WPT_SSH_CONNECT ) ) {
+			$WPT_PHPUNIT_CMD = 'ssh ' . $WPT_SSH_OPTIONS . ' ' . escapeshellarg( $WPT_SSH_CONNECT ) . ' ' . escapeshellarg( $WPT_PHPUNIT_CMD );
+		}
+
+		// Execute the PHPUnit command.
+		perform_operations( array(
+			$WPT_PHPUNIT_CMD
+		) );
+
+	}
+} else {
+	
+	/**
+	 * Determines the PHPUnit command to execute the test suite.
+	 * Retrieves the PHPUnit command from the environment variable 'WPT_PHPUNIT_CMD'. If the environment
+	 * variable is not set or is empty, it constructs a default command using the PHP executable path and
+	 * the test directory path from environment variables, appending parameters to the PHPUnit call to
+	 * avoid reporting useless tests.
+	 */
+	$WPT_PHPUNIT_CMD = trim( getenv( 'WPT_PHPUNIT_CMD' ) );
+	if( empty( $WPT_PHPUNIT_CMD ) ) {
+		$WPT_PHPUNIT_CMD = 'cd ' . escapeshellarg( $WPT_TEST_DIR ) . ' && ' . $WPT_PHP_EXECUTABLE . ' ./vendor/phpunit/phpunit/phpunit --dont-report-useless-tests' . $WPT_FLAVOR_TXT . $WPT_EXTRATESTS_TXT;
+	}
+
+	// If an SSH connection string is provided, prepend the SSH command to the PHPUnit execution command.
+	if ( ! empty( $WPT_SSH_CONNECT ) ) {
+		$WPT_PHPUNIT_CMD = 'ssh ' . $WPT_SSH_OPTIONS . ' ' . escapeshellarg( $WPT_SSH_CONNECT ) . ' ' . escapeshellarg( $WPT_PHPUNIT_CMD );
+	}
+
+	// Execute the PHPUnit command.
+	perform_operations( array(
+		$WPT_PHPUNIT_CMD
+	) );
+
+}
+	
